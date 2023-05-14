c77c78b243fcbdc3b2d0824cbbcca0f2c1cf4318
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-05-14 15:25:31 +0300

    Доавил методов в класс

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -14,6 +14,24 @@ class RegistrationPage:
     def fill_last_name(self, value):
         browser.element('#lastName').type(value)
 
+    def fill_email(self, value):
+        browser.element('#userEmail').type(value)
+
+    def choose_gender(self, value):
+        browser.all('[name = gender]').element_by(have.value(value)).element('..').click()
+
+    def fill_mobile_number(self, value):
+        browser.element('#userNumber').type(value)
+
+    def birth_date_choose(self, value):
+        browser.element('#dateOfBirthInput').perform(command.select_all).type(value).press_enter()
+
+    def subject_choose(self, value):
+        browser.element('#subjectsInput').type(value).press_enter()
+
+    def hobbies_choose(self, value):
+        browser.all('[for^=hobbies-checkbox]').element_by(have.text(value)).click()
+
 
 def test_registration_form(browser_start):
     registration_page = RegistrationPage()
@@ -22,14 +40,17 @@ def test_registration_form(browser_start):
     # WHEN
     registration_page.fill_first_name('Daniil')
     registration_page.fill_last_name('Moiseenko')
+    registration_page.fill_email('test@test.test')
+    registration_page.choose_gender('Male')
+    registration_page.fill_mobile_number('78484884844')
+    registration_page.birth_date_choose('07 Apr 1990')
+    registration_page.subject_choose('Computer Science')
+    registration_page.hobbies_choose('Reading')
 
-    browser.element('#userEmail').type('test@test.test')
-    browser.element('[for = gender-radio-1]').click()
-    browser.element('#userNumber').type('78484884844')
-    browser.element('#dateOfBirthInput').perform(command.select_all).type('07 Apr 1990').press_enter()
-    browser.element('#subjectsInput').type('Computer Science').press_enter()
-    browser.element('[for = hobbies-checkbox-2]').click()
+
     browser.element('#uploadPicture').set_value(resource.path('Imp.jpg'))
+
+
     browser.element('#currentAddress').type('Test adress')
     browser.element('#state').click().with_().element('#react-select-3-option-0').click()
     browser.element('#city').click().with_().element('#react-select-4-option-0').click()
