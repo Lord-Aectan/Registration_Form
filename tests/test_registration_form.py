15653e3477b425302770c17d7bdc7ad0c31bcfb4
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-05-14 16:35:47 +0300

    Для заполнения оставил только методы, проверку ещё не трогал

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,6 +1,5 @@
 from selene import browser, have, command
 from registration_form import resource
-from tests import conftest
 
 
 class RegistrationPage:
@@ -23,14 +22,31 @@ class RegistrationPage:
     def fill_mobile_number(self, value):
         browser.element('#userNumber').type(value)
 
-    def birth_date_choose(self, value):
+    def choose_birth_date(self, value):
         browser.element('#dateOfBirthInput').perform(command.select_all).type(value).press_enter()
 
-    def subject_choose(self, value):
+    def choose_subject(self, value):
         browser.element('#subjectsInput').type(value).press_enter()
 
-    def hobbies_choose(self, value):
+    def choose_hobbies(self, value):
         browser.all('[for^=hobbies-checkbox]').element_by(have.text(value)).click()
+
+    def upload_picture(self, value):
+        browser.element('#uploadPicture').set_value(resource.path(value))
+
+    def fill_adress(self, value):
+        browser.element('#currentAddress').type(value)
+
+    def choose_state(self, value):
+        browser.element('#state').click()
+        browser.all('[id^=react-select][id*=option]').element_by(have.text(value)).click()
+
+    def choose_city(self, value):
+        browser.element('#city').click()
+        browser.all('[id^=react-select][id*=option]').element_by(have.text(value))
+
+    def submit(self):
+        browser.element('#submit').click()
 
 
 def test_registration_form(browser_start):
@@ -43,25 +59,22 @@ def test_registration_form(browser_start):
     registration_page.fill_email('test@test.test')
     registration_page.choose_gender('Male')
     registration_page.fill_mobile_number('78484884844')
-    registration_page.birth_date_choose('07 Apr 1990')
-    registration_page.subject_choose('Computer Science')
-    registration_page.hobbies_choose('Reading')
+    registration_page.choose_birth_date('07 Apr 1990')
+    registration_page.choose_subject('Computer Science')
+    registration_page.choose_hobbies('Reading')
+    registration_page.upload_picture('Imp.jpg')
+    registration_page.fill_adress('Test adress')
+    registration_page.choose_state('NCR')
+    registration_page.choose_city('Delhi')
+    registration_page.submit()
 
-
-    browser.element('#uploadPicture').set_value(resource.path('Imp.jpg'))
-
-
-    browser.element('#currentAddress').type('Test adress')
-    browser.element('#state').click().with_().element('#react-select-3-option-0').click()
-    browser.element('#city').click().with_().element('#react-select-4-option-0').click()
-    browser.element('#submit').click()
 
     # THEN
-    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
-    browser.all('tbody tr').should(have.exact_texts(
-        'Student Name Test Test2', 'Student Email test@test.test',
-        'Gender Male', 'Mobile 7848488484',
-        'Date of Birth 07 April,1990', 'Subjects Computer Science',
-        'Hobbies Reading', 'Picture Imp.jpg',
-        'Address Test adress', 'State and City NCR Delhi'
-    ))
+   # browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+   # browser.all('tbody tr').should(have.exact_texts(
+   #     'Student Name Test Test2', 'Student Email test@test.test',
+   #     'Gender Male', 'Mobile 7848488484',
+   #     'Date of Birth 07 April,1990', 'Subjects Computer Science',
+  #      'Hobbies Reading', 'Picture Imp.jpg',
+  #      'Address Test adress', 'State and City NCR Delhi'
+  #  ))
