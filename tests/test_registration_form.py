0f5f17bd0497996580fe591829e4651a7bfa3b96
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-05-15 21:39:53 +0300

    создал и описал экземпляр класса

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,7 +1,23 @@
+from registration_form.data.users import User, Gender, Subject, Hobbies
 from registration_form.pages.registration_page import RegistrationPage
 
 
 def test_registration_form(browser_start):
+    student = User(
+        first_name ='Daniil',
+        last_name='Moiseenko',
+        email='test@test.test',
+        gender=Gender.male.value,
+        mobile_number='78484884844',
+        birth_day='07 Apr 1990',
+        subjects=Subject.computer_science.value,
+        hobbies=Hobbies.reading.value,
+        picture='Imp.jpg',
+        current_address='Test address',
+        state='Haryana',
+        city='Panipat'
+    )
+
     registration_page = RegistrationPage()
     registration_page.open()
 
